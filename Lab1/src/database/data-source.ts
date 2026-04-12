30e2c9853b091a3320c7882fe7354d503c26c4be
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2026-04-12 15:27:54 +0300

    fix(data-source): update migrations path to correct directory structure

--- Lab1/src/database/data-source.ts
+++ Lab1/src/database/data-source.ts
@@ -10,5 +10,5 @@ export default new DataSource({
   password: getArg('db-password'),
   database: getArg('db-name'),
   entities: [Note],
-  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
 });
